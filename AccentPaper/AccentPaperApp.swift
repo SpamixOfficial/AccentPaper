2d908f68fcd9cc1a092f44d9c9a0ca3a23ef336d
SpamixOfficial <spamixproducer@gmail.com>
2025-08-06 16:02:22 +0200

    Initial Commit

---
+++ AccentPaper/AccentPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccentPaperApp.swift
+//  AccentPaper
+//
+//  Created by Alexander on 2025-08-06.
+//
+
+import SwiftUI
+
+@main
+struct AccentPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccentPaperTests/AccentPaperTests.swift
@@ -0,0 +1,17 @@
+//
+//  AccentPaperTests.swift
+//  AccentPaperTests
+//
+//  Created by Alexander on 2025-08-06.
+//
+
+import Testing
+@testable import AccentPaper
+
+struct AccentPaperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
